c2e56d8931616ff3f478b583496158a8beb4dba2
fayfaylong <143350494+fayfaylong@users.noreply.github.com>
2023-09-21 14:47:48 -0700

    Add files via upload

--- Address_piechart.py
+++ Address_piechart.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 data = pd.read_csv('USA_Housing.csv')
 
-labels = 'NE', 'CA', 'WI', 'AP', 'AE'
+labels = ['NE', 'CA', 'WI', 'AP', 'AE']
 sizes = [4.09, 3.09, 5.13, 3.26, 4.23]
-fig, ax = plt.subplots()
-ax.pie(sizes, labels=labels, autopct='%1.1f%%')
-ax.set_title('Address_piechart')
 
-st.pyplot(fig)
+fig = px.pie(data_frame=pd.DataFrame({'labels': labels, 'sizes': sizes}), 
+             names='labels', 
+             values='sizes', 
+             title='Address_piechart')
+
+st.plotly_chart(fig)
